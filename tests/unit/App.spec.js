3a0cabff192bd0681d0d8d72f0705bae6551ba0c
Your Name <you@example.com>
2023-11-08 10:56:59 -0500

    lint

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,40 +1,44 @@
-import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
+import { shallowMount } from "@vue/test-utils";
+import App from "@/App.vue";
 global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve({ items: [], totalItems: 50 }),
   })
 );
 
-beforeEach(() => {  
+beforeEach(() => {
   fetch.mockClear();
 });
 
-jest.mock('@/components/BookSearchInput.vue', () => ({
-  name: 'BookSearchInput',
-  render: (h) => h('div'),
+jest.mock("@/components/BookSearchInput.vue", () => ({
+  name: "BookSearchInput",
+  render: (h) => h("div"),
 }));
-jest.mock('@/components/BookList.vue', () => ({
-  name: 'BookList',
-  render: (h) => h('div'),
+jest.mock("@/components/BookList.vue", () => ({
+  name: "BookList",
+  render: (h) => h("div"),
 }));
-jest.mock('@/components/HomePagination.vue', () => ({
-  name: 'HomePagination',
-  render: (h) => h('div'),
+jest.mock("@/components/HomePagination.vue", () => ({
+  name: "HomePagination",
+  render: (h) => h("div"),
 }));
 
-describe('App.vue', () => {
-  it('initializes with correct elements', () => {
+describe("App.vue", () => {
+  it("initializes with correct elements", () => {
     const wrapper = shallowMount(App);
-    expect(wrapper.findComponent({ name: 'BookSearchInput' }).exists()).toBe(true);
-    expect(wrapper.findComponent({ name: 'BookList' }).exists()).toBe(true);
-    expect(wrapper.findComponent({ name: 'HomePagination' }).exists()).toBe(true);
+    expect(wrapper.findComponent({ name: "BookSearchInput" }).exists()).toBe(
+      true
+    );
+    expect(wrapper.findComponent({ name: "BookList" }).exists()).toBe(true);
+    expect(wrapper.findComponent({ name: "HomePagination" }).exists()).toBe(
+      true
+    );
   });
-  
-  it('calls fetchBooks and fetchTotalResults when searchBooks is called', async () => {
+
+  it("calls fetchBooks and fetchTotalResults when searchBooks is called", async () => {
     const wrapper = shallowMount(App);
-    const fetchBooksSpy = jest.spyOn(wrapper.vm, 'fetchBooks');
-    const fetchTotalResultsSpy = jest.spyOn(wrapper.vm, 'fetchTotalResults');
+    const fetchBooksSpy = jest.spyOn(wrapper.vm, "fetchBooks");
+    const fetchTotalResultsSpy = jest.spyOn(wrapper.vm, "fetchTotalResults");
 
     await wrapper.vm.searchBooks();
 
@@ -42,10 +46,10 @@ describe('App.vue', () => {
     expect(fetchTotalResultsSpy).toHaveBeenCalled();
     fetchBooksSpy.mockRestore();
     fetchTotalResultsSpy.mockRestore();
-  });  
-  it('updates currentPage and calls fetchBooks when changePage is called', async () => {
+  });
+  it("updates currentPage and calls fetchBooks when changePage is called", async () => {
     const wrapper = shallowMount(App);
-    const fetchBooksSpy = jest.spyOn(wrapper.vm, 'fetchBooks');
+    const fetchBooksSpy = jest.spyOn(wrapper.vm, "fetchBooks");
     wrapper.vm.changePage(2);
     await wrapper.vm.$nextTick();
 
